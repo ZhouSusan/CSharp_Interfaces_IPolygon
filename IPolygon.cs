63f8d6b72be313f8d31fc041a0f2dbc1fe5d8d02
ZhouSusan <szhou089@gmail.com>
2022-12-11 08:03:56 -0800

    Added calculatePerimeter() method to Ipolygon interface and implemented code to Reactangle class

--- IPolygon.cs
+++ IPolygon.cs
@@ -4,6 +4,7 @@ namespace CSharp_Interfaces_IPolygon
 {
     interface IPolygon
     {
+        void calculatePerimeter(int c, int d);
         void calculateArea(int a, int b);
     }
 }
--- Rectangle.cs
+++ Rectangle.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace CSharp_Interfaces_IPolygon
 {
-    internal class Class1
+    class Rectangle : IPolygon
     {
+        public void calculateArea(int a, int b)
+        {
+            int area = a * b;
+            Console.WriteLine($"Area of a rectangle is {area}.");
+        }
+
+        public void calculatePerimter(int c, int d)
+        {
+            int perimeter = 2*(c + d);
+            Console.WriteLine($"Perimer of a rectangle is {perimeter}.");
+        }
     }
 }
